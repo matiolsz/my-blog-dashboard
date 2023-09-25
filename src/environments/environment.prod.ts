9d739978564d52df273ecc72ae1b9ea6856dd9a2
matiolsz <matiolsz97@gmail.com>
2023-09-25 22:11:28 +0200

    - add firebase

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyD8nYexa0C44Yyu8JHeodEiqD26jCOB13Q',
+    authDomain: 'todo-900e2.firebaseapp.com',
+    projectId: 'todo-900e2',
+    storageBucket: 'todo-900e2.appspot.com',
+    messagingSenderId: '355838894718',
+    appId: '1:355838894718:web:bb3097d2f501c3f0feae2f',
+    measurementId: 'G-HYJZB5FVEF',
+  },
+};
